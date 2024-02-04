c6239aaedac3c209c0092da46e9a54c500321903
oliveiraphm <oliveiraphm@gmail.com>
2024-02-04 15:34:40 -0300

    refatorando o teste

---
+++ tests/test_app.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from time import sleep
+
+import pytest
+import subprocess
+
+@pytest.fixture
+def driver():
+    # Iniciar o Streamlit em background
+    process = subprocess.Popen(["streamlit", "run", "src/app.py"])
+
+    # Iniciar o WebDriver usando GeckoDriver
+    driver = webdriver.Firefox()
+    driver.set_page_load_timeout(5)
+    yield driver
+
+    # Fechar o WebDriver e o Streamlit após o teste
+    driver.quit()
+    process.kill()
+
+def test_app_opens(driver):
+    # Verificar se a página abre
+    driver.get("http://localhost:8501")
+    sleep(5)
